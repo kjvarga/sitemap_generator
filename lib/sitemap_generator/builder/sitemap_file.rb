b7b5519fc2ea0a363bd31fcedadc316bfa72d241
Stav <stav@ftbpro.com>
2014-07-14 18:54:25 +0300

    Updated with the changes for sitemap_file.rb

--- lib/sitemap_generator/builder/sitemap_file.rb
+++ lib/sitemap_generator/builder/sitemap_file.rb
@@ -19,33 +19,34 @@ module SitemapGenerator
       #
       # * <tt>location</tt> - a SitemapGenerator::SitemapLocation instance or a Hash of options
       #   from which a SitemapLocation will be created for you.
-      def initialize(opts={})
+      def initialize(opts={}, schemas)
         @location = opts.is_a?(Hash) ? SitemapGenerator::SitemapLocation.new(opts) : opts
         @link_count = 0
         @news_count = 0
         @xml_content = '' # XML urlset content
+        @schemas = schemas
         @xml_wrapper_start = <<-HTML
-          <?xml version="1.0" encoding="UTF-8"?>
-            <urlset
-              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-              xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-              xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-              xmlns:image="#{SitemapGenerator::SCHEMAS['image']}"
-              xmlns:video="#{SitemapGenerator::SCHEMAS['video']}"
-              xmlns:geo="#{SitemapGenerator::SCHEMAS['geo']}"
-              xmlns:news="#{SitemapGenerator::SCHEMAS['news']}"
-              xmlns:mobile="#{SitemapGenerator::SCHEMAS['mobile']}"
-              xmlns:pagemap="#{SitemapGenerator::SCHEMAS['pagemap']}"
-              xmlns:xhtml="http://www.w3.org/1999/xhtml"
-            >
+          "#{all_schemas}"
         HTML
         @xml_wrapper_start.gsub!(/\s+/, ' ').gsub!(/ *> */, '>').strip!
+        @xml_wrapper_start = @xml_wrapper_start.slice(1..-2)
         @xml_wrapper_end   = %q[</urlset>]
         @filesize = SitemapGenerator::Utilities.bytesize(@xml_wrapper_start) + SitemapGenerator::Utilities.bytesize(@xml_wrapper_end)
         @written = false
         @reserved_name = nil # holds the name reserved from the namer
         @frozen = false      # rather than actually freeze, use this boolean
+      end
+
+      def all_schemas
+        xml_start = '<?xml version="1.0" encoding="UTF-8"?>
+            <urlset
+              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+              xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+              xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" '
+        xml_schemas = (@schemas.collect do |schema, content|  "xmlns:#{schema}=\"#{content}\"\n" end).join(" ")
+        xml_end = 'xmlns:xhtml="http://www.w3.org/1999/xhtml" >'
+        return xml_start + xml_schemas + xml_end
       end
 
       # If a name has been reserved, use the last modified time from the file.
@@ -167,3 +168,4 @@ module SitemapGenerator
     end
   end
 end
+
