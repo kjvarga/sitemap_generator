2eb802ab353124435f4fa2ed077837d63f0f8b57
Nicolas Rodriguez <nico@nicoladmin.fr>
2024-10-31 02:37:14 +0100

    Cleanup spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,19 @@
+# Load dev/test libs
+require 'byebug'
+require 'webmock/rspec'
+
+# Load support files
+require_relative 'support/file_macros'
+require_relative 'support/xml_macros'
+
+# Load simplecov
 # require 'simplecov'
 # SimpleCov.start
-require 'bundler/setup'
-Bundler.require
 
-require './spec/support/file_macros'
-require './spec/support/xml_macros'
-require 'webmock/rspec'
-require 'byebug'
-
+# Configure webmock
 WebMock.disable_net_connect!
 
-SitemapGenerator.verbose = false
-
+# Configure rspec
 RSpec.configure do |config|
   config.include(FileMacros)
   config.include(XmlMacros)
@@ -20,7 +22,15 @@ RSpec.configure do |config|
   config.order = :random
   Kernel.srand config.seed
 
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
   # disable monkey patching
   # see: https://rspec.info/features/3-12/rspec-core/configuration/zero-monkey-patching-mode/
   config.disable_monkey_patching!
 end
+
+# Load our own gem
+require 'sitemap_generator'
+SitemapGenerator.verbose = false
